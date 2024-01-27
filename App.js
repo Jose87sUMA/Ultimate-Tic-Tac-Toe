fbd1f5ca2d8c9b0340fd4af3d559277aa8494df0
nikasenia <niks@uma.es>
2024-01-27 19:36:28 +0100

    Created two news classes, moved app code

--- App.js
+++ App.js
@@ -1,63 +1,30 @@
 // App.js
 import {React, useState} from 'react';
-import { SafeAreaView, StyleSheet, Text, Button } from 'react-native';
-import BigBoard from './BigBoard';
-import Game from './Game';
-import monteCarloTreeSearch from './MonteCarloTreeSearch.js';
+import { SafeAreaView, View, StyleSheet, Text, Button } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const initialGame = new Game('O');
-  const [gameInstance, setGame] = useState(initialGame);
-
-  const onPressCell = (smallBoardIndex, pos) => {
-    console.log(`Cell pressed: smallBoardIndex=${smallBoardIndex}, pos=${pos}`);
-
-    let newGameInstance = gameInstance.clone();
-
-    if (newGameInstance.makeMove(smallBoardIndex, pos)){
-      setGame(newGameInstance);
-    }
-
-    setTimeout(() => {
-      // Trigger AI move after a delay
-      AIMove();
-    }, 500); // 500 milliseconds delay as an example    
-  }
-
-  const AIMove = async () => {
-    await new Promise(resolve => setTimeout(resolve, 500));
-
-    setGame(prevGame => {
-      const newState = monteCarloTreeSearch(prevGame, 10);
-      const newGame = Game.fromState(newState);
-      return newGame;
-    });
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Ultimate Tic-Tac-Toe</Text>
-      <BigBoard bigBoard={gameInstance.bigBoard} winnerBoard={gameInstance.winnerBoard} boardOfNextMove={gameInstance.boardOfNextMove} onPressCell={onPressCell} AITurn={gameInstance.AITurn}/>
-      <Text style={styles.title}>Menu</Text>
-      
-    </SafeAreaView> 
-  );
-};
+    <NavigationContainer>
+      <Stack.Navigator>
+         <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 50,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-});
+};
 
 export default App;
