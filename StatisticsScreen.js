6593ce577b1b23978dee62f6ee4186726d097a2b
nikasenia <niks@uma.es>
2024-02-02 19:17:37 +0100

    Cleaned up a little

--- StatisticsScreen.js
+++ StatisticsScreen.js
@@ -8,20 +8,12 @@ import MyLightTheme from './styles/MyLightTheme';
 
 const StatisticsScreen = () => {
   const name = 'Jose';
-   
-  //const theme = Appearance.getColorScheme();
-  
-  const { setTheme, theme } = useContext(ThemeContext);
-  //const MyTheme = theme == 'dark'? MyDarkTheme: MyLightTheme;
   const {colors} = useTheme();
-  //const themeStyle = theme == 'dark'? MyDarkTheme: MyLightTheme;
-  
+ 
 
-  
-  
   return (
   <View><Text  style={{ color: colors.text}}> Welcome to the Statistics ! _
-  {theme} This is the theme"!</Text>
+      This is the theme"!</Text>
   </View>
   );
 };
